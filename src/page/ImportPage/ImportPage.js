63556433c53251db122519109fd8975391b58614
suonan <2280347020@qq.com>
2022-04-11 00:16:20 +0800

    nothing

--- src/page/ImportPage/ImportPage.js
+++ src/page/ImportPage/ImportPage.js
@@ -1,6 +1,6 @@
 import './ImportPage.css';
 import Header from '../../component/Header/Header';
-import { Input, Button, Upload } from 'antd';
+import { Input, Button, Upload, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 const onChange = e => {
     console.log('Change:', e.target.value);
@@ -17,47 +17,47 @@ function ImportPage() {
         onChange(info) {
             const { status } = info.file;
             if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
+                console.log(info.file, info.fileList);
             }
             if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
+                message.success(`${info.file.name} file uploaded successfully.`);
             } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
+                message.error(`${info.file.name} file upload failed.`);
             }
         },
         onDrop(e) {
             console.log('Dropped files', e.dataTransfer.files);
-            },
+        },
     };
     return (
         <div>
-            <Header keyValue="import"/>
+            <Header keyValue="import" />
             <div className='border'>
-                <Input className='InputBox' addonBefore="bid     " style={{width: '90%'}} placeholder="bid" />
-                <Input className='InputBox' addonBefore="category" style={{width: '90%'}} placeholder="category" />
-                <Input className='InputBox' addonBefore='title   ' style={{width: '90%'}} placeholder="title" />
-                <Input className='InputBox' addonBefore='press   ' style={{width: '90%'}} placeholder="press" />
-                <Input className='InputBox' addonBefore='year    ' style={{width: '90%'}} placeholder="year" />
-                <Input className='InputBox' addonBefore='author  ' style={{width: '90%'}} placeholder="author" />
-                <Input className='InputBox' addonBefore='price   ' style={{width: '90%'}} placeholder="price" />
-                <Input className='InputBox' addonBefore='number  ' style={{width: '90%'}} placeholder="number" />
-                
+                <Input className='InputBox' addonBefore="bid     " style={{ width: '90%' }} placeholder="bid" />
+                <Input className='InputBox' addonBefore="category" style={{ width: '90%' }} placeholder="category" />
+                <Input className='InputBox' addonBefore='title   ' style={{ width: '90%' }} placeholder="title" />
+                <Input className='InputBox' addonBefore='press   ' style={{ width: '90%' }} placeholder="press" />
+                <Input className='InputBox' addonBefore='year    ' style={{ width: '90%' }} placeholder="year" />
+                <Input className='InputBox' addonBefore='author  ' style={{ width: '90%' }} placeholder="author" />
+                <Input className='InputBox' addonBefore='price   ' style={{ width: '90%' }} placeholder="price" />
+                <Input className='InputBox' addonBefore='number  ' style={{ width: '90%' }} placeholder="number" />
+
                 <Button className='Button1' type="primary" >
-                        ADD
+                    ADD
                 </Button>
             </div>
             <div className='border'>
                 <Dragger {...props} >
                     <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
+                        <InboxOutlined />
                     </p>
                     <p className="ant-upload-text">Click or drag file to this area to upload</p>
                     <p className="ant-upload-hint">
-                    Upload a .txt to import the book
+                        Upload a .txt to import the book
                     </p>
                 </Dragger>
             </div>
-            
+
         </div>
     )
 }
