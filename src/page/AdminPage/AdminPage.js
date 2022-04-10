23d975b1144dfc10b9265a4c2a599012b21897ab
刘奕骁 <liuyixiao0821@gmail.com>
2022-04-10 16:52:15 +0800

    reconstruct the admin page

--- src/page/AdminPage/AdminPage.js
+++ src/page/AdminPage/AdminPage.js
@@ -1,25 +1,48 @@
-import './AdminPage.css';
-import Header from '../../component/Header/Header';
-import { Input, Button } from 'antd';
-import { EyeInvisibleOutlined, EyeTwoTone, UserOutlined, LockOutlined, CreditCardOutlined } from '@ant-design/icons';
+import "./AdminPage.css";
+import Header from "../../component/Header/Header";
+import { Input, Button } from "antd";
+import {
+  EyeInvisibleOutlined,
+  EyeTwoTone,
+  UserOutlined,
+  LockOutlined,
+  CreditCardOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+
 function AdminPage() {
-    return (
-        <div>
-            <Header keyValue="admin" />
-            <div className='border'>
-                <Input className='InputBox'
-                    placeholder="username" prefix={<UserOutlined />} />
-                <Input.Password className='InputBox'
-                    placeholder="password" prefix={<LockOutlined />} />
-                <Button className='Button' type="primary">
-                    Sign in
-                </Button> 
-            </div>            
-        </div>
-
-    )
+  const [isLogin, setIsLogin] = useState(false);
+  let login = (
+    <div className="border">
+      <Input
+        className="InputBox"
+        placeholder="username"
+        prefix={<UserOutlined />}
+      />
+      <Input.Password
+        className="InputBox"
+        placeholder="password"
+        prefix={<LockOutlined />}
+      />
+      <Button
+        className="Button"
+        type="primary"
+        onClick={() => {
+          // TODO: judge if login
+          setIsLogin(true);
+        }}
+      >
+        Sign in
+      </Button>
+    </div>
+  );
+  let adminInfo = <div>雷猴哇，唔系管理员</div>;
+  return (
+    <div>
+      <Header keyValue="admin" />
+      {isLogin ? adminInfo : login}
+    </div>
+  );
 }
-
-
 
 export default AdminPage;
