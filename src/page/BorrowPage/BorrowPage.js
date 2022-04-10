6c670995e8f4183a03dc550a77a57d611c426e1a
suonan <2280347020@qq.com>
2022-04-10 21:16:58 +0800

    roughly finish borrow, but 打包 still not finished

--- src/page/BorrowPage/BorrowPage.js
+++ src/page/BorrowPage/BorrowPage.js
@@ -2,22 +2,25 @@ import './BorrowPage.css';
 import Header from '../../component/Header/Header';
 import { Input, Button, DatePicker } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone, UserOutlined, LockOutlined, CreditCardOutlined, BookOutlined, CalendarOutlined } from '@ant-design/icons';
-const onChange = e => {
-    console.log('Change:', e.target.value);
-};
+import { useState } from "react";
+import moment from "moment";
 function BorrowPage() {
+    const [bno, setBno] = useState("");
+    const [cno, setCno] = useState("");
+    const [date, setDate] = useState(["", ""]);// return like: ['2022-04-30', '2022-05-31'] 打包部分仍未完成
+
     return (
         <div>
             <Header keyValue="borrow" />
             <div className='border'>
-                <Input className='InputBox' showCount maxLength={7} onChange={onChange}  placeholder="cno" prefix={<CreditCardOutlined />} />
-                <Input className='InputBox' showCount maxLength={8} onChange={onChange}  placeholder="bno" prefix={<BookOutlined />} />
-                <DatePicker.RangePicker className='InputBox'/>
+                <Input className='InputBox' showCount maxLength={7} onChange={(event) => { setCno(event.target.value) }} placeholder="cno" prefix={<CreditCardOutlined />} />
+                <Input className='InputBox' showCount maxLength={8} onChange={(event) => { setBno(event.target.value) }} placeholder="bno" prefix={<BookOutlined />} />
+                <DatePicker.RangePicker className='InputBox' format="YYYY-MM-DD" onChange={(date, dateString) => { setDate(dateString); }} />
                 <Button className='Button' type="primary">
                     Borrow
-                </Button> 
+                </Button>
             </div>
-            
+
         </div>
     )
 }
