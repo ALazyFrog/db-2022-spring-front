09dbe5d131bf5bb9124900eb69daf67f696587d8
suonan <2280347020@qq.com>
2022-04-11 11:03:24 +0800

    fix data type in import

--- src/page/ImportPage/ImportPage.js
+++ src/page/ImportPage/ImportPage.js
@@ -44,28 +44,29 @@ function ImportPage() {
                 <Input className='InputBox' addonBefore="category" style={{ width: '90%' }} placeholder="category" onChange={(event) => { setCategory(event.target.value) }} />
                 <Input className='InputBox' addonBefore='title   ' style={{ width: '90%' }} placeholder="title" onChange={(event) => { setTitle(event.target.value) }} />
                 <Input className='InputBox' addonBefore='press   ' style={{ width: '90%' }} placeholder="press" onChange={(event) => { setPress(event.target.value) }} />
-                <Input className='InputBox' addonBefore='year    ' style={{ width: '90%' }} placeholder="year" onChange={(event) => { setYear(event.target.value) }} />
+                <Input className='InputBox' addonBefore='year    ' style={{ width: '90%' }} placeholder="year" onChange={(event) => { setYear(parseInt(event.target.value, 10)) }} />
                 <Input className='InputBox' addonBefore='author  ' style={{ width: '90%' }} placeholder="author" onChange={(event) => { setAuthor(event.target.value) }} />
-                <Input className='InputBox' addonBefore='price   ' style={{ width: '90%' }} placeholder="price" onChange={(event) => { setPrice(event.target.value) }} />
-                <Input className='InputBox' addonBefore='number  ' style={{ width: '90%' }} placeholder="number" onChange={(event) => { setNumber(event.target.value) }} />
+                <Input className='InputBox' addonBefore='price   ' style={{ width: '90%' }} placeholder="price" onChange={(event) => { setPrice(parseFloat(event.target.value)) }} />
+                <Input className='InputBox' addonBefore='number  ' style={{ width: '90%' }} placeholder="number" onChange={(event) => { setNumber(parseInt(event.target.value, 10)) }} />
 
                 <Button className='Button1' type="primary" onClick={() => {
                     let data = {
-                        category : category,
+                        category: category,
                         title: title,
                         press: press,
                         year: year,
                         author: author,
                         price: price,
-                        number: number
+                        total: number,
+                        stock: number
                     }
-                    request("/book","POST",data).then(
+                    request("/book", "POST", data).then(
                         (response) => {
                             console.log(response);
                             alert(response.message)
                         }
                     )
-                 }}>
+                }}>
                     ADD
                 </Button>
             </div>
