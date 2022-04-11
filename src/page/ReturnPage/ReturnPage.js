a0d7bb0c4a675773a7bb1ab8ac1044136c54252e
TomisTony <1158176767@qq.com>
2022-04-11 19:31:03 +0800

    finish return page

--- src/page/ReturnPage/ReturnPage.js
+++ src/page/ReturnPage/ReturnPage.js
@@ -1,12 +1,71 @@
 import './ReturnPage.css';
 import Header from '../../component/Header/Header';
-import { Input, Button } from 'antd';
+import { Input, Button, Table } from 'antd';
 import { CreditCardOutlined, BookOutlined } from '@ant-design/icons';
 import { useState } from "react";
+import request from '../../util/request';
+
+const columns = [
+    {
+      title: 'bid',
+      dataIndex: 'bid',
+      sorter: (a, b) => a.bid - b.bid,
+      defaultSortOrder: 'descend',
+    },
+    {
+      title: 'category',
+      dataIndex: 'category',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.category - b.category,
+    },
+    {
+      title: 'title',
+      dataIndex: 'title',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.title - b.title,
+    },
+    {
+      title: 'press',
+      dataIndex: 'press',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.press - b.press,
+    },
+    {
+      title: 'year',
+      dataIndex: 'year',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.year - b.year,
+    },
+    {
+      title: 'author',
+      dataIndex: 'author',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.author - b.author,
+    },
+    {
+      title: 'price',
+      dataIndex: 'price',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: 'total',
+      dataIndex: 'total',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.total - b.total,
+    },
+    {
+      title: 'stock',
+      dataIndex: 'stock',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.stock - b.stock,
+    },
+  ];
 
 function ReturnPage() {
     const [bno, setBno] = useState("");
     const [cno, setCno] = useState("");
+    const [data, setData] = useState([]);
     return (
         <div>
             <Header keyValue="return" />
@@ -15,11 +74,30 @@ function ReturnPage() {
                 <Input className='InputBox' showCount maxLength={8} onChange={(event) => { setBno(event.target.value) }} placeholder="bno" prefix={<BookOutlined />} />
 
                 <Button className='Button' type="primary" onClick={() => {
-                    // TODO: 留一个提交数据的坑位 直接使用bno cno即可
+                if (bno != "" && cno != "") {
+                    request("/borrow/"+cno+"/"+bno, 'PUT').then(
+                    (response) => {
+                        alert(response.message)
+                    }
+                    )
+                } else if (cno != "" && bno == "") {
+                    request("/borrow/" + cno, 'GET',).then(
+                    (response) => {
+                        if (response.code != 0) {
+                        alert(response.message)
+                        } else {
+                        setData(response.data)
+                        console.log(response.data)
+                        }
+
+                    }
+                    )
+                }
                 }}>
-                    Return
+                Return
                 </Button>
             </div>
+            <Table className='table' columns={columns} dataSource={data} />
         </div>
     )
 }
